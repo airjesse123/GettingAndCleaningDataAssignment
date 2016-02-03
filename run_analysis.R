f831ced65fa294d0d5a24aaa0b88435d6e68cc70
Jesse Unger <jesse.m.unger@gmail.com>
2016-02-02 16:21:54 -0800

    completed assignment

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,82 @@
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
-filepath  <-file.path(getwd(),"FGDP.csv")
-download.file(url, filepath, mode = "wb")
-dtGDP <- data.table(read.csv(filepath, skip = 4, nrows = 215))
-dtGDP <- dtGDP[X != ""]
-dtGDP <- dtGDP[, list(X, X.1, X.3, X.4)]
-setnames(dtGDP, c("X", "X.1", "X.3", "X.4"), c("CountryCode", "rankingGDP", 
-                                               "Long.Name", "gdp"))
+# get the data.table library
+library(data.table)
+library(dplyr)
 
-url2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
-filepath2  <-file.path(getwd(),"FGDP.csv")
-download.file(url2, filepath2, mode = "wb")
-dtEd <- data.table(read.csv(filepath2))
-dt <- merge(dtGDP, dtEd, all = TRUE, by = c("CountryCode"))
-sum(!is.na(unique(dt$rankingGDP)))
+# set wd and show files in that dir
+setwd("U:/GettingAndCleaningDataAssignment")
+filepath  <- file.path(getwd(),"UCI HAR Dataset")
+list.files(filepath, recursive = TRUE)
 
-dt[order(rankingGDP, decreasing = TRUE), list(CountryCode, Long.Name.x, Long.Name.y,rankingGDP, gdp)][13]
+# get test data from that directory and create tables from it
+testPath <- file.path(filepath,"test/subject_test.txt")
+testYPath <- file.path(filepath,"test/y_test.txt")
+testXPath <- file.path(filepath,"test/X_test.txt")
+testTable<-read.table(testPath)
+testXTable<-read.table(testXPath)
+testYTable<-read.table(testYPath)
 
-dt[, mean(rankingGDP, na.rm = TRUE), by = Income.Group]
+# get test subject numbers
+names(testTable)<-"Subject"
+testData<-data.table(testTable)
 
-cuts <- quantile(dt$rankingGDP, probs = c(0.0, 0.2,0.4,0.6,0.8, 1), na.rm = TRUE)
-dt$quantileGDP <- cut(dt$rankingGDP, breaks = cuts)
-dt[Income.Group == "Lower middle income", .N, by = c("Income.Group", "quantileGDP")]
+#get test features and crop down to just mean and std
+featuresPath <- file.path(filepath,"features.txt")
+featuresTable <- read.table(featuresPath)
+featuresLogical <- grepl("mean|std", featuresTable$V2)
+names(testXTable)=featuresTable$V2
+testXTableNew<-testXTable[,featuresLogical]
+testXData<-data.table(testXTableNew)
+
+#get test activities and add a column with their name
+activitiesPath <- file.path(filepath,"activity_labels.txt")
+activitiesTable <- read.table(activitiesPath)
+names(activitiesTable)=c("ActivityNumber","ActivityName")
+testYTableNew <- testYTable
+testYTableNew[,2] <- activitiesTable$ActivityName[testYTable[,1]]
+names(testYTableNew)=c("ActivityNumber","ActivityName")
+testYData<-data.table(testYTableNew)
+
+#bind together all test above information
+allTest<-cbind(testData,testXData,testYData)
+
+# get train data from that directory and create tables from it
+trainPath <- file.path(filepath,"train/subject_train.txt")
+trainYPath <- file.path(filepath,"train/y_train.txt")
+trainXPath <- file.path(filepath,"train/X_train.txt")
+trainTable<-read.table(trainPath)
+trainXTable<-read.table(trainXPath)
+trainYTable<-read.table(trainYPath)
+
+# get train subject numbers
+names(trainTable)<-"Subject"
+trainData<-data.table(trainTable)
+
+#get train features and crop down to just mean and std
+featuresPath <- file.path(filepath,"features.txt")
+featuresTable <- read.table(featuresPath)
+featuresLogical <- grepl("mean|std", featuresTable$V2)
+names(trainXTable)=featuresTable$V2
+trainXTableNew<-trainXTable[,featuresLogical]
+trainXData<-data.table(trainXTableNew)
+
+#get train activities and add a column with their name
+activitiesPath <- file.path(filepath,"activity_labels.txt")
+activitiesTable <- read.table(activitiesPath)
+names(activitiesTable)=c("ActivityNumber","ActivityName")
+trainYTableNew <- trainYTable
+trainYTableNew[,2] <- activitiesTable$ActivityName[trainYTable[,1]]
+names(trainYTableNew)=c("ActivityNumber","ActivityName")
+trainYData<-data.table(trainYTableNew)
+
+#bind together all train above information
+allTrain<-cbind(trainData,trainXData,trainYData)
+
+#merge together test and train
+allData<-rbind(allTest,allTrain)
+
+#query the tidy data ussing aggregate
+tidyData = aggregate(allData, by=list(Subject=allData$Subject,ActivityName = allData$ActivityName), mean)
+tidyData[,84]=NULL
+tidyData[,83]=NULL
+tidyData[,3]=NULL
+write.table(tidyData, "tidyData.txt", sep="|")
